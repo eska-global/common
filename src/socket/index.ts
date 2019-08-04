3253b1aecbdf938ec298ca0cc66e21468a6bca68
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-08-04 18:47:39 +0300

    types

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,12 @@
 import './controller/TestController';
-import './server/web';
-import './server/types';
 
 export { IOSocketServer } from './server/io';
+export { WebSocketServer } from './server/web';
+export {
+    IEmitter, SocketType, Listeners,
+    AnyFunction, SocketRunnable, SocketServer,
+    ApiContainer
+} from './server/types';
 
 // import { IOSocketServer } from '../socket/server/io';
 // import './controller/TestController';
